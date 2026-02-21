b1d674746f0e7544937f0c13f1abfa809bf3379a
amolnavale7 <amolnavale7@gmail.com>
2026-02-21 13:06:43 +0530

    This is my first program to commit

---
+++ my_program.py
@@ -0,0 +1,9 @@
+""""
+print("Hello everyone ! Welcome to Python learning Journey..")
+num=10
+print(num)
+"""
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print("My name is: ",name,"and","My age is: ", age)
